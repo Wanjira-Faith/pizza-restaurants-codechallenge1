7a2b90fd98db411c3292a84454085c67d465a0e4
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-09-23 12:00:45 +0300

    defined timestamps for record creation and updates

--- lib/models.py
+++ lib/models.py
@@ -22,3 +22,5 @@ class Pizza(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     ingredients = db.Column(db.String(255))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
