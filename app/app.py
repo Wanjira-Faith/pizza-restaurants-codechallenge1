38ca626e4072fdbd475525f1d1e53b7e4705edac
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-09-23 16:27:14 +0300

    created migrations

--- app/app.py
+++ app/app.py
@@ -1,13 +1,17 @@
 from flask import Flask
-from config import SQLALCHEMY_DATABASE_URI
-from models import db  
+from flask_migrate import Migrate
+
+from models import db
 
 # Create a Flask application instance
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+migrate = Migrate(app, db)
+
+db.init_app(app)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=5555, debug=True)
