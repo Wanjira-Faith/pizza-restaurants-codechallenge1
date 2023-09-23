4dbd92f2bb04aab68a9b69b2b799a851a1b72ee5
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-09-23 14:11:41 +0300

    created migrations

---
+++ migrations/versions/b981c94991fd_first_db_migrate.py
@@ -0,0 +1,24 @@
+"""first db migrate
+
+Revision ID: b981c94991fd
+Revises: 
+Create Date: 2023-09-23 14:10:53.330460
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'b981c94991fd'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
