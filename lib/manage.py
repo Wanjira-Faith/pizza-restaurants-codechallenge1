8f99aa8a16c695b9f0a33d903ff190c888a53259
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-09-23 13:53:26 +0300

    import app

--- lib/manage.py
+++ lib/manage.py
@@ -1,6 +1,8 @@
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-from lib import app, db
+from app import app, db
+
+app = create_app()
 
 # Create a Flask-Migrate instance
 migrate = Migrate(app, db)
