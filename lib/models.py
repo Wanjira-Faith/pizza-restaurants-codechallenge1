4dbcee8d9e96ecdf9adce75615a2d8be087fc2b2
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-09-23 11:47:52 +0300

    defined db columns

--- lib/models.py
+++ lib/models.py
@@ -5,4 +5,6 @@ from datetime import datetime
 db = SQLAlchemy()
 
 class Restaurant(db.Model):
-    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    address = db.Column(db.String(255))
