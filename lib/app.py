b46c64619142155b90193c728d2c03513a9d3979
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-09-23 13:11:22 +0300

    initialize flask app with sqlalchemy db

--- lib/app.py
+++ lib/app.py
@@ -6,3 +6,5 @@ from models import db
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
