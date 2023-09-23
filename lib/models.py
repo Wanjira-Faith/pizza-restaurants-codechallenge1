4e0894c490f19f499f16075966f30c08e6a13beb
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-09-23 12:08:15 +0300

    defined RestautantPizza model

--- lib/models.py
+++ lib/models.py
@@ -30,4 +30,7 @@ class Pizza(db.Model):
         self.ingredients = ingredients
 
     def __repr__(self):
-        return f'<Pizza {self.id}: {self.name}'    
+        return f'<Pizza {self.id}: {self.name}'   
+
+class RestaurantPizza(db.Model):
+         
