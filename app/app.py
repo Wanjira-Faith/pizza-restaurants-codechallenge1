cc0567af6569e5e7cbe11b5ec2890993d76316ef
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-09-23 18:48:32 +0300

    updated file

--- app/app.py
+++ app/app.py
@@ -14,4 +14,4 @@ migrate = Migrate(app, db)
 db.init_app(app)
 
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    app.run(debug=True)
--- config.py
+++
@@ -1,4 +0,0 @@
-import os
-
-SQLALCHEMY_DATABASE_URI = 'sqlite:///pizza_restaurants.db' 
-SQLALCHEMY_TRACK_MODIFICATIONS = False
