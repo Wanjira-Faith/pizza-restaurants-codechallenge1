ebe7aafaf26a5129fefa77f3da85171d81404370
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-09-23 12:49:09 +0300

    removed the timestamps for restaurant model

--- lib/models.py
+++ lib/models.py
@@ -8,9 +8,7 @@ class Restaurant(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), unique=True, nullable=False)
     address = db.Column(db.String(255))
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
-
+    
     def __init__(self, name, address):
         self.name = name
         self.address = address
