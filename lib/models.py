671dd7318d2a7d4b501c2f6f1cb6b36129f257f2
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-09-23 12:18:34 +0300

    added foreign keys to associate a restaurant and a pizza

--- lib/models.py
+++ lib/models.py
@@ -35,3 +35,5 @@ class Pizza(db.Model):
 class RestaurantPizza(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     price = db.Column(db.Float(CheckConstraint('price >= 1 AND price <= 30')), nullable=False)
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id'), nullable=False)
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizza.id'), nullable=False)
