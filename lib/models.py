f5dd20e3baff88098d480f55513d6ce512b2d4aa
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-09-23 12:06:36 +0300

    custom string representation for debugging and logging

--- lib/models.py
+++ lib/models.py
@@ -28,3 +28,6 @@ class Pizza(db.Model):
     def __init__(self, name, ingredients):
         self.name = name
         self.ingredients = ingredients
+
+    def __repr__(self):
+        return f'<Pizza {self.id}: {self.name}'    
