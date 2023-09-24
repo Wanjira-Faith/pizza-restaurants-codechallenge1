5ac13c0a517581ac04f0081370e1a2530bcfae3a
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-09-24 11:47:02 +0300

    defined route to get all restaurants

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_migrate import Migrate
 from models import db, Restaurant, Pizza, RestaurantPizza
 
@@ -14,7 +14,22 @@ migrate = Migrate(app, db)
 # Initialize database with Flask app
 db.init_app(app)
 
-from routes import *
+# Route to get all restaurants
+@app.route('/restaurants', methods=['GET'])
+def get_restaurants():
+    restaurants = Restaurant.query.all()
+    restaurant_list = []
+
+    for restaurant in restaurants:
+        restaurant_data = {
+            'id': restaurant.id,
+            'name': restaurant.name,
+            'address': restaurant.address
+        }
+
+        restaurant_list.append(restaurant_data)
+
+    return jsonify(restaurant_list), 200    
 
 if __name__ == '__main__':
     app.run(debug=True)
